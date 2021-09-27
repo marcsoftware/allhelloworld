6ed63348fa7df14a70b195692be34d8f6acc602e
marcsoftware <marcdavidmelcher@gmail.com>
2021-09-27 12:56:23 -0500

    works

---
+++ javahello/Hello.java
@@ -0,0 +1,12 @@
+package javahello;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello,vvfff World!"); 
+        
+        ArrayList<String> cars = new ArrayList<>(); // Create an ArrayList object
+    }
+}
