c3c273c5ea95ba90e711e0f4f4aeb89f417e46b4
marcsoftware <marcdavidmelcher@gmail.com>
2022-01-24 13:00:18 -0600

    .

--- javahello/Hello.java
+++
@@ -1,9 +0,0 @@
-package javahello;
-import java.util.ArrayList;
-class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello,vvfff World!"); 
-        
-        ArrayList<String> cars = new ArrayList<>(); // Create an ArrayList object
-    }
-}
---
+++ javahello/javahello/Hello.java
@@ -0,0 +1,22 @@
+package javahello;
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Hello
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.get(0);
+        arrayList.size();
+        
+        Iterator<Integer> iterator = arrayList.iterator();
+
+ 
+
+
+    }///////////////////////////////////////////////////////////////////////////
+}
