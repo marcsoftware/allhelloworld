1a3ab939260cfa02df74ee4563b33cb762a06a1d
marcsoftware <marcdavidmelcher@gmail.com>
2022-01-26 15:08:19 -0600

    mysql notes

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -57,8 +57,78 @@ insert into albums(name, release_year,band_id)
     ('power slace', 1984,1),
     ('nightmare',2018,2),
     ('nightmare',2010,3),
-    ('test album',NULL,3);
+    ('test album',NULL,3); 
 
 SELECT * from albums;
-31:49
+select name from albums;
+select distinct name from album ;
+update albums set relsear_year = 1982;//wrong changes all records :(
+    update albums set release_year = 1982
+        where id=1;
+
+select * from albums where
+relsease<2000;
+
+
+select * from albums
+where name like '%er%';
+
+select * from albums
+where name like '%er%'or band_id=2;
+
+
+select * from albums
+where name release_year = 1984 and band_id=1;
+
+select * from albums
+where relsear_year between 2000 and 2018;
+
+select * from albums 
+where release_year is null;
+
+delete from albums where id = 5;
+
+select * from bands
+join albums on bands.id=albums.band_id;
+//select when equal
+
+join
+inner join 
+left join // returns left side even if no match
+right join // returns right side even if no match
+
+select avg(relsease_year) from albums;
+select SUM(relsease_year) from albums;
+select band_id, count(band_id) from albums
+group by band_id; // squish matching band_id in to one
+
+excersies in video
+https://github.com/WebDevSimplified/Learn-SQL
+
+select b.name as band_name, count(a.id) as num_albums
+from bands as b 
+left join albums as a on b.id=a.band_id
+group by b.id;
+
+select b.name as band_name, count(a.id) as num_albums
+from bands as b 
+left join albums as a on b.id=a.band_id
+where num_albums =1 // wont work since where happends after group by
+group by b.id;
+
+
+select b.name as band_name, count(a.id) as num_albums
+from bands as b 
+left join albums as a on b.id=a.band_id
+group by b.id
+having num_albums =1;
+
+
+select b.name as band_name, count(a.id) as num_albums
+from bands as b 
+left join albums as a on b.id=a.band_id
+where b.name='deuce'
+group by b.id
+having num_albums =1;
+
 
